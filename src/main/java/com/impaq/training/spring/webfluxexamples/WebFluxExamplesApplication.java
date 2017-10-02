136976a47e40802e90a49d8fbdad26c7c4c694ff
Lukasz Soszynski <lsoszyn@gmail.com>
2017-10-02 21:00:15 +0200

    Generated project with postgres configuration

---
+++ src/main/java/com/impaq/training/spring/webfluxexamples/WebFluxExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.impaq.training.spring.webfluxexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxExamplesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/impaq/training/spring/webfluxexamples/WebFluxExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.impaq.training.spring.webfluxexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebFluxExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
